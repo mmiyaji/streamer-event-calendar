3a6c065484b5b8cfe21f7c6efd0eaeeeb6da72a7
mmiyaji <ruhenheim1223@gmail.com>
2026-08-26 19:06:12 +0900

    chore(build): load automation event data

--- scripts/build.py
+++ scripts/build.py
@@ -10,6 +10,7 @@ SHADOWVERSE_EVENTS_PATH = ROOT / "data" / "shadowverse_wb_events.json"
 DISCOVERED_EVENTS_PATH = ROOT / "data" / "discovered_events.json"
 STREAMER_LEAGUE_EVENTS_PATH = ROOT / "data" / "streamer_league_events.json"
 SHADOWVERSE_OCS_EVENTS_PATH = ROOT / "data" / "shadowverse_ocs_events.json"
+AUTOMATION_EVENTS_PATH = ROOT / "data" / "automation_events.json"
 EVENT_OVERRIDES_PATH = ROOT / "data" / "event_overrides.json"
 SFL_SCHEDULE_PATH = ROOT / "data" / "sfl_2026_schedule.json"
 SITE_DIR = ROOT / "site"
@@ -180,6 +181,7 @@ def main() -> None:
         (DISCOVERED_EVENTS_PATH, load_events(DISCOVERED_EVENTS_PATH)),
         (STREAMER_LEAGUE_EVENTS_PATH, load_events(STREAMER_LEAGUE_EVENTS_PATH)),
         (SHADOWVERSE_OCS_EVENTS_PATH, load_events(SHADOWVERSE_OCS_EVENTS_PATH)),
+        (AUTOMATION_EVENTS_PATH, load_events(AUTOMATION_EVENTS_PATH)),
         (EVENT_OVERRIDES_PATH, load_events(EVENT_OVERRIDES_PATH)),
         (SFL_SCHEDULE_PATH, build_sfl_events(SFL_SCHEDULE_PATH)),
     ]
