323b8dd8103e3e93a33b75ddaaeb7b8bee1a2e81
mmiyaji <ruhenheim1223@gmail.com>
2026-09-04 19:09:58 +0900

    fix(calendar): add Kato to PUBG LAND team

--- scripts/build.py
+++ scripts/build.py
@@ -12,6 +12,7 @@ STREAMER_LEAGUE_EVENTS_PATH = ROOT / "data" / "streamer_league_events.json"
 SHADOWVERSE_OCS_EVENTS_PATH = ROOT / "data" / "shadowverse_ocs_events.json"
 AUTOMATION_EVENTS_PATH = ROOT / "data" / "automation_events.json"
 AUTOMATION_DELTA_EVENTS_PATH = ROOT / "data" / "automation_events_delta.json"
+AUTOMATION_LATEST_EVENTS_PATH = ROOT / "data" / "automation_events_latest.json"
 EVENT_OVERRIDES_PATH = ROOT / "data" / "event_overrides.json"
 SFL_SCHEDULE_PATH = ROOT / "data" / "sfl_2026_schedule.json"
 SITE_DIR = ROOT / "site"
@@ -184,6 +185,7 @@ def main() -> None:
         (SHADOWVERSE_OCS_EVENTS_PATH, load_events(SHADOWVERSE_OCS_EVENTS_PATH)),
         (AUTOMATION_EVENTS_PATH, load_events(AUTOMATION_EVENTS_PATH)),
         (AUTOMATION_DELTA_EVENTS_PATH, load_events(AUTOMATION_DELTA_EVENTS_PATH)),
+        (AUTOMATION_LATEST_EVENTS_PATH, load_events(AUTOMATION_LATEST_EVENTS_PATH)),
         (EVENT_OVERRIDES_PATH, load_events(EVENT_OVERRIDES_PATH)),
         (SFL_SCHEDULE_PATH, build_sfl_events(SFL_SCHEDULE_PATH)),
     ]
