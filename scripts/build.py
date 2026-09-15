5a5d8a369302e4372a033ee67dd9f5ecd17415f0
mmiyaji <ruhenheim1223@gmail.com>
2026-09-15 19:01:32 +0900

    feat(calendar): include TGS automation events

--- scripts/build.py
+++ scripts/build.py
@@ -14,6 +14,7 @@ AUTOMATION_EVENTS_PATH = ROOT / "data" / "automation_events.json"
 AUTOMATION_DELTA_EVENTS_PATH = ROOT / "data" / "automation_events_delta.json"
 AUTOMATION_LATEST_EVENTS_PATH = ROOT / "data" / "automation_events_latest.json"
 AUTOMATION_INBOX_EVENTS_PATH = ROOT / "data" / "automation_events_inbox.json"
+AUTOMATION_TGS_EVENTS_PATH = ROOT / "data" / "automation_events_tgs.json"
 EVENT_OVERRIDES_PATH = ROOT / "data" / "event_overrides.json"
 SFL_SCHEDULE_PATH = ROOT / "data" / "sfl_2026_schedule.json"
 SITE_DIR = ROOT / "site"
@@ -190,6 +191,7 @@ def main() -> None:
         (EVENT_OVERRIDES_PATH, load_events(EVENT_OVERRIDES_PATH)),
         (AUTOMATION_LATEST_EVENTS_PATH, load_events(AUTOMATION_LATEST_EVENTS_PATH)),
         (AUTOMATION_INBOX_EVENTS_PATH, load_events(AUTOMATION_INBOX_EVENTS_PATH)),
+        (AUTOMATION_TGS_EVENTS_PATH, load_events(AUTOMATION_TGS_EVENTS_PATH)),
         (SFL_SCHEDULE_PATH, build_sfl_events(SFL_SCHEDULE_PATH)),
     ]
     events = merge_events(event_groups)
